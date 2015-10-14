6f7b110c1b9fa4f8b538130d9285f88076a5a84b
Sashi Kumar <ksashikumark93@gmail.com>
2015-10-15 00:06:01 +0530

    Add GameActivity and DBHandler

---
+++ src/com/pivotxstudios/thirtysecondchallenge/GameActivity.java
@@ -0,0 +1,26 @@
+package com.pivotxstudios.thirtysecondchallenge;
+
+import java.io.IOException;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class GameActivity extends SimpleBaseGameActivity {
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
+
+    @Override
+    protected void onCreateResources() throws IOException {
+
+    }
+
+    @Override
+    protected Scene onCreateScene() {
+        return null;
+    }
+
+}
